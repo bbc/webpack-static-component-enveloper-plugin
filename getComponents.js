02a3eb2f7140950edc4e9cd70749e91bcb4e261e
Luke Ewer <luke.ewer@bbc.co.uk>
2019-04-08 16:59:10 +0100

    Add linting

--- getComponents.js
+++ getComponents.js
@@ -1,43 +1,43 @@
-const fs = require('fs');
+const fs = require('fs')
 
-const getFilesInDirectory = async ({directory}) => {
-    return new Promise((resolve) => {
-        fs.readdir(directory, (err, files) => {
-            resolve(files);
-        });
-    });
-};
+const getFilesInDirectory = async ({ directory }) => {
+  return new Promise((resolve, reject) => {
+    fs.readdir(directory, (err, files) => {
+      if (err) reject(err)
+      else resolve(files)
+    })
+  })
+}
 
-const getComponentName = ({componentName, componentFile}) => {
-    if (typeof componentName === 'string') {
-        return componentName;
+const getComponentName = ({ componentName, componentFile }) => {
+  if (typeof componentName === 'string') {
+    return componentName
+  } else if (typeof componentName === 'function') {
+    const name = componentName(componentFile)
+    if (typeof name === 'string') {
+      return name
     }
-    else if (typeof componentName === 'function') {
-        const name = componentName(componentFile);
-        if (typeof name === 'string') {
-            return name;
-        }
-        throw new Error ('Component name function must return a string.');
+    throw new Error('Component name function must return a string.')
+  }
+  throw new Error('Component name must be a string or function.')
+}
+
+const getComponents = async ({ directory, test, componentName }) => {
+  const filesInSrcDirectory = await getFilesInDirectory({ directory })
+
+  const matchedComponentFiles = filesInSrcDirectory.filter((file) => {
+    return typeof test === 'function' ? test(file) : test.test(file)
+  })
+
+  const components = matchedComponentFiles.map((componentFile) => {
+    const name = getComponentName({ componentName, componentFile })
+    return {
+      name: name,
+      file: componentFile
     }
-    throw new Error ('Component name must be a string or function.');
-};
+  })
 
-const getComponents = async ({directory, test, componentName}) => {
-    const filesInSrcDirectory = await getFilesInDirectory({directory});
+  return components
+}
 
-    const matchedComponentFiles = filesInSrcDirectory.filter((file) => {
-        return typeof test === 'function' ? test(file) : test.test(file);
-    });
-
-    const components = matchedComponentFiles.map((componentFile) => {
-        const name = getComponentName({componentName, componentFile});
-        return {
-            name: name,
-            file: componentFile
-        }
-    });
-
-    return components;
-};
-
-module.exports = getComponents;
+module.exports = getComponents
